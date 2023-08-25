5ba274a6ee87b987acd1c95e11565fb2a9d2f600
James Gallagher <jgallagher@opendap.org>
2023-08-25 10:29:40 -0600

    Update README, add process-logs.sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
-= Performance analysis of NASA's Open Altimetry Web UI =
+# Performance analysis of NASA's Open Altimetry Web UI
 
-== Driving around on UAT's EC2 instances ==
+## Driving around on UAT's EC2 instances
 
 AWS Console --> System Manager --> Session Manager. 
 
@@ -12,7 +12,7 @@ Once there (useful commands):
 * `sudo -s` gets root access
 * `docker ps -a` shows the container Ids
 * `docker exec it <container> bash` to access the container with a Bash shell
-* `docker cp <conatiner>:/var/log/bes/bes.log .` when run in the EC2 host (not the dontainer) will copy the container's bes.log to the EC2 host's file system
+* `docker cp <conatiner>:/var/log/bes/bes.log .` when run in the EC2 host (not the container) will copy the container's bes.log to the EC2 host's file system
 * `aws s3 cp bes.log s3://opendap.scratch/UAT-logs/bes-2d47.log` How to get files off of an EC2 instance in UAT, et cetera. Use ENV Vars for the credentials.
 
 
---
+++ raw-bes-logs/process-logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# extract information from bes log files and prepare them to be read into a spreadsheet
+
+# $1 is the log file name
+# $2 is the basename for the new, subset log files
+# writes the log file as a CSV to stdout
+
+# basic stats
+
+echo "Total lines, $(wc -l $1)"
+echo "Error lines, $(grep '|error|' $1 | wc -l)"
+echo "Request lines, $(grep '|request|' $1 | wc -l)"
+echo "Info lines, $(grep '|info|' $1 | wc -l)"
+echo "Timing lines, $(grep '|timing|' $1 | wc -l)"
+
+# Split into four sub logs, error, request, info and timing.
+# Turn them into CSV files
+# Sort those by PID and then 
+
+grep '|error|' $1 | sed 's@|&|@,@g' | sort --numeric-sort --field-separator=, --key=2 > $2.error.csv
+grep '|request|' $1 | sed 's@|&|@,@g' | sort --numeric-sort --field-separator=, --key=2 > $2.request.csv
+grep '|info|' $1 | sed 's@|&|@,@g' | sort --numeric-sort --field-separator=, --key=2 > $2.info.csv
+grep '|timing|' $1 | sed 's@|&|@,@g' | sort --numeric-sort --field-separator=, --key=2 > $2.timing.csv
